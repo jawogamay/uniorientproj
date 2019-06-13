716ae42332693164745c7f09a032c4161a3cc8ee
jawogamay <ninomasbang@gmail.com>
2019-06-13 14:29:12 -0700

    Removed

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -15,9 +15,9 @@
     <!-- Bootstrap Core CSS -->
     <link href="/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- chartist CSS -->
-    <link href="/assets/plugins/chartist-js/dist/chartist.min.css" rel="stylesheet">
+   {{--  <link href="/assets/plugins/chartist-js/dist/chartist.min.css" rel="stylesheet">
     <link href="/assets/plugins/chartist-js/dist/chartist-init.css" rel="stylesheet">
-    <link href="/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css" rel="stylesheet">
+    <link href="/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css" rel="stylesheet"> --}}
     <!--This page css - Morris CSS -->
     <link href="/assets/plugins/c3-master/c3.min.css" rel="stylesheet">
     <!-- Custom CSS -->
@@ -287,31 +287,31 @@ th{
     <!-- ============================================================== -->
     <script src="/js/app.js"></script>
     <!-- Bootstrap tether Core JavaScript -->
-    <script src="/assets/plugins/popper/popper.min.js"></script>
-    <script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+  {{--   <script src="/assets/plugins/popper/popper.min.js"></script>
+    <script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script> --}}
     <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="/js/jquery.slimscroll.js"></script>
+ {{--    <script src="/js/jquery.slimscroll.js"></script> --}}
     <!--Wave Effects -->
-    <script src="/js/waves.js"></script>
+    {{-- <script src="/js/waves.js"></script> --}}
     <!--Menu sidebar -->
     <script src="/js/sidebarmenu.js"></script>
     <!--stickey kit -->
-    <script src="/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+   {{--  <script src="/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
     <script src="/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
-    <script src="/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <script src="/assets/plugins/sparkline/jquery.sparkline.min.js"></script> --}}
     <!--Custom JavaScript -->
     <script src="/js/custom.min.js"></script>
     <!-- ============================================================== -->
     <!-- This page plugins -->
     <!-- ============================================================== -->
     <!-- chartist chart -->
-    <script src="/assets/plugins/chartist-js/dist/chartist.min.js"></script>
-    <script src="/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
+   {{--  <script src="/assets/plugins/chartist-js/dist/chartist.min.js"></script>
+    <script src="/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script> --}}
     <!--c3 JavaScript -->
-    <script src="/assets/plugins/d3/d3.min.js"></script>
-    <script src="/assets/plugins/c3-master/c3.min.js"></script>
+   {{--  <script src="/assets/plugins/d3/d3.min.js"></script>
+    <script src="/assets/plugins/c3-master/c3.min.js"></script> --}}
     <!-- Chart JS -->
-    <script src="/js/dashboard6.js"></script>
+   {{--  <script src="/js/dashboard6.js"></script> --}}
     <!-- ============================================================== -->
     <!-- Style switcher -->
     <!-- ============================================================== -->
